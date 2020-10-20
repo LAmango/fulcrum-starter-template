db7fcbe263ce5c01492652242b427627164f793e
Lucas Albano <lucas@albano.us>
2020-10-20 10:27:16 -0400

    added storybook

--- src/components/common/button.js
+++ src/components/common/button.js
@@ -1,8 +1,54 @@
 import React from "react"
+import PropTypes from "prop-types"
 
-const Button = props => {
-  const { children, ...other } = props
-  return <button {...other}>{children}</button>
+const Button = ({
+                  label,
+                  onClick,
+                  primary,
+                  size,
+                  color,
+                  stretch,
+                  className,
+                  ...props
+                }) => {
+  return (
+    <button
+      className={`
+      button
+      border
+      border-${color}
+      ${stretch ? "w-full" : ""}
+      ${
+        primary
+          ? `button-primary bg-${color} hover:bg-white`
+          : `button-default bg-white hover:bg-${color}`
+      }
+      button-${size}
+      `}
+      onClick={onClick}
+      {...props}
+    >
+      {label}
+    </button>
+  )
+}
+
+Button.propTypes = {
+  primary: PropTypes.bool,
+  stretch: PropTypes.bool,
+  /**
+   * must have the color and intensity, ex: `blue-300`
+   */
+  color: PropTypes.string,
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+}
+
+Button.defaultProps = {
+  primary: true,
+  color: "black",
+  size: "small",
 }
 
 export default Button
